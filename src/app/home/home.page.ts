19a8a85912610292ab06bb2ed2752f73c10ca7a8
Fly <ak.fly81@hotmail.com>
2024-10-05 19:13:33 +0200

    Reestructured index page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,32 +1,124 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonGrid, IonRow, IonCol, IonCard, IonCardContent, IonCardTitle, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardContent,
+  IonCardTitle,
+  IonSelect,
+  IonSelectOption
+} from '@ionic/angular/standalone';
+
+interface Card {
+  id: number;
+  name: string;
+  rarity: string;
+  imagePath: string;
+}
+
+interface GameSet {
+  id: number;
+  name: string;
+  imagePath: string;
+  cardList: Card[];
+}
+
+interface Game {
+  id: number;
+  name: string;
+  coinName: string;
+  coinImagePath: string;
+  setList: GameSet[];
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonCardTitle, IonCardContent, IonCard, IonCol, IonRow, IonGrid, IonLabel, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, IonSelect, IonSelectOption],
+  imports: [
+    CommonModule,
+    IonCardTitle,
+    IonCardContent,
+    IonCard,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonLabel,
+    IonItem,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonSelect,
+    IonSelectOption
+  ]
 })
 export class HomePage {
   title: string = 'My game';
-  games: string[] = ['Game 1', 'Game 2', 'Game 3'];
-  selectedGames: string[] = [];
-  gridItems: { image: string, name: string, progress: string }[] = [];
+  games: Game[] = [
+    {
+      id: 1,
+      name: 'Game 1',
+      coinName: 'Coin 1',
+      coinImagePath: 'path/to/coin1.png',
+      setList: [
+        {
+          id: 1,
+          name: 'Set 1',
+          imagePath: 'path/to/set1.png',
+          cardList: [
+            { id: 1, name: 'Card 1', rarity: 'Common', imagePath: 'path/to/card1.png' },
+            { id: 2, name: 'Card 2', rarity: 'Rare', imagePath: 'path/to/card2.png' }
+          ]
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Game 2',
+      coinName: 'Coin 2',
+      coinImagePath: 'path/to/coin2.png',
+      setList: [
+        {
+          id: 2,
+          name: 'Set 2',
+          imagePath: 'path/to/set2.png',
+          cardList: [
+            { id: 3, name: 'Card 3', rarity: 'Common', imagePath: 'path/to/card3.png' },
+            { id: 4, name: 'Card 4', rarity: 'Rare', imagePath: 'path/to/card4.png' }
+          ]
+        }
+      ]
+    }
+  ];
+  selectedGame: Game | null = null;
+  gridItems: { image: string; name: string; progress: string }[] = [];
 
   constructor() {}
 
   onGameSelectionChange(event: any) {
-    this.selectedGames = event.target.value;
+    this.selectedGame = event.target.value;
     this.updateGridItems();
   }
 
   updateGridItems() {
-    // Example logic to update grid items based on selected games
-    this.gridItems = this.selectedGames.map(game => ({
-      image: 'path/to/image.png',
-      name: game,
-      progress: '0/150'
-    }));
+    if (this.selectedGame) {
+      this.gridItems = this.selectedGame.setList.map(set => ({
+        image: set.imagePath,
+        name: set.name,
+        progress: `0/${set.cardList.length}`
+      }));
+    } else {
+      this.gridItems = [];
+    }
   }
 }
